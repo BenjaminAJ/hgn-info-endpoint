7bb78bb563771c2933e4a8bd9014319e8cbaef9f
BenjaminAJ <ajanakub30@gmail.com>
2023-09-08 17:25:57 +0100

    Implement UTC time validation in project endpoint for accuracy

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ const port = process.env.PORT || 5500;
 app.get('/api/',(req,res)=>{    
 
     const {slack_name, track} = req.query;
+
+     // Get the current day of the week
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const current_day = daysOfWeek[new Date().getDay()];
+
+    const now = new Date();    
+    const utc_time = now.toISOString();
+    
+
     const jsonResponse = {
         slack_name,
-        current_day: "Monday",
-        utc_time: "2023-08-21T15:04:05Z",
+        current_day,
+        utc_time,
         track,
         github_file_url: "https://github.com/BenjaminAJ/hgn-info-endpoint/blob/main/index.js",
         github_repo_url: "https://github.com/BenjaminAJ/hgn-info-endpoint",
